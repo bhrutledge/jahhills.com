99d2d66375316f55b00c0d5a43a3cfc7819bcab9
Brian Rutledge <bhrutledge@gmail.com>
2014-09-12 07:55:34 -0400

    Split up acceptance tests

---
+++ hth/tests/test_news.py
@@ -0,0 +1,31 @@
+from time import sleep
+
+from core.tests.selenium import SeleniumTestCase
+from news.tests.factories import PostFactory
+from news.models import Post
+
+class NewsTestCase(SeleniumTestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        PostFactory.create_batch(10, publish=True)
+        PostFactory.create_batch(5)
+
+    def test_news_shows_all_published_posts(self):
+        self.get_url('/news')
+        self.assertIn('News', self.browser.title)
+
+        live_titles = [p.text for p in self.find_css('.post .title')]
+        published_titles = [p.title for p in Post.objects.published()]
+
+        self.assertEqual(live_titles, published_titles)
+
+    # def test_home_shows_most_recent_post(self):
+    #     self.get_url('')
+    #
+    #     live_titles = [p.text for p in self.find_css('.post .title')]
+    #     published_titles = [p.title for p in Post.objects.published()]
+    #
+    #     self.assertEqual(len(live_titles), 1)
+    #     self.assertEqual(live_title[0], published_titles[0])
--- hth/tests/test_site.py
+++ hth/tests/test_site.py
@@ -1,16 +1,8 @@
 from time import sleep
 
 from core.tests.selenium import SeleniumTestCase
-from news.tests.factories import PostFactory
-from news.models import Post
 
 class SiteTestCase(SeleniumTestCase):
-
-    def setUp(self):
-        super().setUp()
-
-        PostFactory.create_batch(10, publish=True)
-        PostFactory.create_batch(5)
 
     def test_can_access_site(self):
         self.get_url('')
@@ -33,20 +25,3 @@ class SiteTestCase(SeleniumTestCase):
         self.get_url('/videos')
         self.assertIn('Videos', self.browser.title)
 
-    def test_news_shows_all_published_posts(self):
-        self.get_url('/news')
-        self.assertIn('News', self.browser.title)
-
-        live_titles = [p.text for p in self.find_css('.post .title')]
-        published_titles = [p.title for p in Post.objects.published()]
-
-        self.assertEqual(live_titles, published_titles)
-
-    # def test_home_shows_most_recent_post(self):
-    #     self.get_url('')
-    #
-    #     live_titles = [p.text for p in self.find_css('.post .title')]
-    #     published_titles = [p.title for p in Post.objects.published()]
-    #
-    #     self.assertEqual(len(live_titles), 1)
-    #     self.assertEqual(live_title[0], published_titles[0])
