79e0add760b3302ff25d3f9655011ff1cac39359
Brian Rutledge <bhrutledge@gmail.com>
2014-11-14 19:33:29 -0500

    Display latest post on news page, close #33

--- hth/hth/tests/test_news.py
+++ hth/hth/tests/test_news.py
@@ -14,16 +14,13 @@ class NewsTestCase(SeleniumTestCase):
 
         self.latest_post = self.published_posts[0]
 
-        # Test Markdown rendering
-        self.latest_post.body = "- item 1\n- item 2\n- item 3\n"
-        self.latest_post.save()
-
     def test_news_detail_displays_entire_post(self):
         self.get_url(self.latest_post.get_absolute_url())
 
         self.assertEqual(self.find_css('.post .title')[0].text,
                          self.latest_post.title)
-        self.assertEqual(self.find_css('.post .body li')[0].text, 'item 1')
+        self.assertEqual(self.find_css('.post .body p')[0].text,
+                         self.latest_post.body)
 
     def test_news_displays_published_post_titles(self):
         self.get_url('/news')
@@ -34,11 +31,20 @@ class NewsTestCase(SeleniumTestCase):
 
         self.assertEqual(displayed_titles, published_titles)
 
+    def test_news_displays_latest_post(self):
+        self.get_url('/news')
+
+        displayed_bodies = [x.text for x in self.find_css('.post .body p')]
+
+        self.assertEqual(len(displayed_bodies), 1)
+        self.assertEqual(displayed_bodies[0], self.latest_post.body)
+
     def test_home_displays_latest_post(self):
         self.get_url('')
 
         displayed_titles = [x.text for x in self.find_css('.post .title')]
+        displayed_bodies = [x.text for x in self.find_css('.post .body p')]
 
         self.assertEqual(len(displayed_titles), 1)
         self.assertEqual(displayed_titles[0], self.latest_post.title)
-        self.assertEqual(self.find_css('.post .body li')[0].text, 'item 1')
+        self.assertEqual(displayed_bodies[0], self.latest_post.body)
