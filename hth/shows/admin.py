bd829504b614d9d3df6fba7e8984543398172bf5
Brian Rutledge <bhrutledge@gmail.com>
2016-07-17 20:53:11 -0400

    Update content

--- hth/shows/admin.py
+++ hth/shows/admin.py
@@ -3,18 +3,21 @@ from django.contrib import admin
 from .models import Venue, Gig
 
 
+@admin.register(Venue)
 class VenueAdmin(admin.ModelAdmin):
     save_on_top = True
     fields = ('name', 'city', 'website')
 
-admin.site.register(Venue, VenueAdmin)
+    search_fields = ('name', 'city')
 
 
+@admin.register(Gig)
 class GigAdmin(admin.ModelAdmin):
     save_on_top = True
     fields = ('date', 'venue', 'description', 'details',
               'publish', 'publish_on',)
     raw_id_fields = ('venue',)
-    list_display = ('date', 'venue',)
 
-admin.site.register(Gig, GigAdmin)
+    date_hierarchy = 'date'
+    list_display = ('date', 'venue', 'publish')
+    search_fields = ('venue__name', 'venue__city')
