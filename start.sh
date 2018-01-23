a0c1ab087453f97e5409a5dc06cb50a954983ee0
Brian Rutledge <bhrutledge@gmail.com>
2018-01-22 21:30:12 -0500

    Add gunicorn start script

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Production settings for www.hallelujahthehills.com on webfaction
+# Assuming PATH is set by supervisor
+
+gunicorn \
+    --pythonpath "${BASH_SOURCE%/*}/hth" \
+    --env DJANGO_SETTINGS_MODULE=hth.settings.prod \
+    --bind 127.0.0.01:13149 \
+    --workers 4 \
+    --access-logfile - \
+    --error-logfile - \
+    hth.wsgi:application
