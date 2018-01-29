6b4bf0a33ac03995b7b430b817055925b0bda640
Brian Rutledge <bhrutledge@gmail.com>
2018-01-28 21:18:04 -0500

    Update start script

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
 # Production settings for www.hallelujahthehills.com on webfaction
-# Assuming PATH is set by supervisor
 
-gunicorn \
+# Assuming PATH is set by supervisor
+# Need `exec` for `supervisorctl stop` to work
+exec gunicorn \
     --pythonpath "${BASH_SOURCE%/*}/hth" \
     --env DJANGO_SETTINGS_MODULE=hth.settings.prod \
     --bind 127.0.0.01:13149 \
